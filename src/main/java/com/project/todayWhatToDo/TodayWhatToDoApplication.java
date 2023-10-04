7df935488b6e2a9a869e572ac39e1700476849f5
yudonggeun <ydong98@gmail.com>
2023-10-04 22:37:16 +0900

    create project

---
+++ src/main/java/com/project/todayWhatToDo/TodayWhatToDoApplication.java
@@ -0,0 +1,13 @@
+package com.project.todayWhatToDo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodayWhatToDoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodayWhatToDoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/todayWhatToDo/TodayWhatToDoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.todayWhatToDo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodayWhatToDoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
