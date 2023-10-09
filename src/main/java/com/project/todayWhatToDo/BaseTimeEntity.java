3057b1e8c54b13041e53cf3cbb4c440f2e78f2f8
zhtmr <zhtmr123@gmail.com>
2023-10-10 01:01:10 +0900

    chore: BaseTimeEntity 파일 위치 수정

--- src/main/java/com/project/todayWhatToDo/BaseTimeEntity.java
+++ src/main/java/com/project/todayWhatToDo/BaseTimeEntity.java
@@ -1,4 +1,4 @@
-package com.project.todayWhatToDo.board;
+package com.project.todayWhatToDo;
 
 import jakarta.persistence.EntityListeners;
 import jakarta.persistence.MappedSuperclass;
--- src/main/java/com/project/todayWhatToDo/board/domain/Board.java
+++ src/main/java/com/project/todayWhatToDo/board/domain/Board.java
@@ -1,6 +1,6 @@
 package com.project.todayWhatToDo.board.domain;
 
-import com.project.todayWhatToDo.board.BaseTimeEntity;
+import com.project.todayWhatToDo.BaseTimeEntity;
 import jakarta.persistence.Entity;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
