6dd105e035452a86d18d1652adbf2cfe8b4a3e67
zhtmr <zhtmr123@gmail.com>
2023-10-10 00:49:17 +0900

    feat: BaseTimeEntity 추가

---
+++ src/main/java/com/project/todayWhatToDo/board/domain/Board.java
@@ -0,0 +1,13 @@
+package com.project.todayWhatToDo.board.domain;
+
+import com.project.todayWhatToDo.board.BaseTimeEntity;
+import jakarta.persistence.Entity;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@Entity
+public class Board extends BaseTimeEntity {
+
+}
