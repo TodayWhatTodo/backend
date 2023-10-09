f46ba5741c213bb9055943a109777dd5d94b54e8
yudonggeun <ydong98@gmail.com>
2023-10-09 23:31:08 +0900

    feat : api document 자동화, security 초기값 설정

---
+++ src/test/java/com/project/todayWhatToDo/IntegrationTest.java
@@ -0,0 +1,9 @@
+package com.project.todayWhatToDo;
+
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@SpringBootTest
+@ActiveProfiles("test")
+public class IntegrationTest {
+}
