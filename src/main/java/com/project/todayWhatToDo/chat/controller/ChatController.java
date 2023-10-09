0eba784d4fba97dbb2d52c79066262ba8513f1cc
zhtmr <zhtmr123@gmail.com>
2023-10-10 00:26:42 +0900

    feat: chat 테스트

---
+++ src/main/java/com/project/todayWhatToDo/chat/controller/ChatController.java
@@ -0,0 +1,26 @@
+package com.project.todayWhatToDo.chat.controller;
+
+import com.project.todayWhatToDo.chat.dto.ChatRoom;
+import com.project.todayWhatToDo.chat.service.ChatService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/chat")
+public class ChatController {
+
+    private final ChatService chatService;
+
+    @PostMapping
+    public ChatRoom createRoom(@RequestParam String name) {
+        return chatService.createRoom(name);
+    }
+
+    @GetMapping
+    public List<ChatRoom> findAllRoom() {
+        return chatService.findAllRoom();
+    }
+}
