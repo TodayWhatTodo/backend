b5114d934cbaafdccb0ff69517a0317a88331fdf
zhtmr <zhtmr123@gmail.com>
2023-10-11 01:47:10 +0900

    fix: cherry-pick

--- src/main/java/com/project/todayWhatToDo/user/domain/User.java
+++ src/main/java/com/project/todayWhatToDo/user/domain/User.java
@@ -66,7 +66,6 @@ public class User {
     @OneToMany(mappedBy = "following", cascade = CascadeType.ALL, orphanRemoval = true)
     private List<Follow> followings = new ArrayList<>();
 
-
     @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
     private List<LikePost> likePosts = new ArrayList<>();
 
