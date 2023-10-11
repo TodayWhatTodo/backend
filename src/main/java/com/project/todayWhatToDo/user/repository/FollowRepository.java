4fc6fdfc69f847562dc9f09489feded797174370
yudonggeun <ydong98@gmail.com>
2023-10-11 14:40:17 +0900

    refactor : 사용하지 않는 import 제거

--- src/main/java/com/project/todayWhatToDo/user/repository/FollowRepository.java
+++ src/main/java/com/project/todayWhatToDo/user/repository/FollowRepository.java
@@ -3,7 +3,6 @@ package com.project.todayWhatToDo.user.repository;
 import com.project.todayWhatToDo.user.domain.Follow;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Range;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
