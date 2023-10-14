ed0d6fa908fbc4975f83a029cba6ddb279007ad2
zhtmr <zhtmr123@gmail.com>
2023-10-14 17:48:34 +0900

    fix: 게시물 좋아요 서비스 수정

--- src/main/java/com/project/todayWhatToDo/post/service/PostLikeService.java
+++ src/main/java/com/project/todayWhatToDo/post/service/PostLikeService.java
@@ -2,10 +2,13 @@ package com.project.todayWhatToDo.post.service;
 
 import com.project.todayWhatToDo.post.domain.LikePost;
 import com.project.todayWhatToDo.post.domain.Post;
+import com.project.todayWhatToDo.post.dto.request.PostRequestDto;
 import com.project.todayWhatToDo.post.exception.PostNotFoundException;
 import com.project.todayWhatToDo.post.repository.PostLikeRepository;
 import com.project.todayWhatToDo.post.repository.PostRepository;
 import com.project.todayWhatToDo.user.domain.User;
+import com.project.todayWhatToDo.user.exception.UserNotFoundException;
+import com.project.todayWhatToDo.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -19,9 +22,11 @@ public class PostLikeService {
 
     private final PostRepository postRepository;
     private final PostLikeRepository postLikeRepository;
+    private final UserRepository userRepository;
 
-    public void likePost(Long postId, User user) {
-        Post post = postRepository.findById(postId).orElseThrow(PostNotFoundException::new);
+    public void likePost(PostRequestDto requestDto) {
+        Post post = postRepository.findById(requestDto.getId()).orElseThrow(PostNotFoundException::new);
+        User user = userRepository.findById(requestDto.getLikedBy()).orElseThrow(UserNotFoundException::new);
         Optional<LikePost> likePost = postLikeRepository.findByPostIdAndUserId(post.getId(), user.getId());
 
         if (likePost.isPresent()) {
