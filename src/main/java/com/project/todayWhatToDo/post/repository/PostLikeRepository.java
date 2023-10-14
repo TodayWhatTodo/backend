5d458613410b3c36463dc6b946f2cf15c584893b
zhtmr <zhtmr123@gmail.com>
2023-10-14 19:45:15 +0900

    chore: 엔티티 이름 변경

--- src/main/java/com/project/todayWhatToDo/post/repository/PostLikeRepository.java
+++ src/main/java/com/project/todayWhatToDo/post/repository/PostLikeRepository.java
@@ -1,11 +1,11 @@
 package com.project.todayWhatToDo.post.repository;
 
-import com.project.todayWhatToDo.post.domain.LikePost;
+import com.project.todayWhatToDo.post.domain.PostLike;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
-public interface PostLikeRepository extends JpaRepository<LikePost, Long> {
+public interface PostLikeRepository extends JpaRepository<PostLike, Long> {
 
-    Optional<LikePost> findByPostIdAndUserId(Long postId, Long UserId);
+    Optional<PostLike> findByPostIdAndUserId(Long postId, Long UserId);
 }
