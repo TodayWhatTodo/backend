59bea52845dbcc35d815b6b7d12a8dcd21ab3147
zhtmr <zhtmr123@gmail.com>
2023-10-15 17:44:54 +0900

    refactor: record 로 변경

--- src/main/java/com/project/todayWhatToDo/post/dto/request/PostRequestDto.java
+++ src/main/java/com/project/todayWhatToDo/post/dto/request/PostRequestDto.java
@@ -2,24 +2,19 @@ package com.project.todayWhatToDo.post.dto.request;
 
 import com.project.todayWhatToDo.post.domain.Post;
 import com.project.todayWhatToDo.post.domain.PostStatus;
-import lombok.AllArgsConstructor;
 import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
 
-@NoArgsConstructor
-@AllArgsConstructor
+
 @Builder
-@Getter
-public class PostRequestDto {
-    private Long id;
-    private String author;
-    private String title;
-    private PostStatus status;
-    private String content;
-    private String category;
-    private Integer like;
-
+public record PostRequestDto (
+        Long id,
+        String author,
+        String title,
+        PostStatus status,
+        String content,
+        String category,
+        Integer like
+) {
 
     public Post toEntity() {
         return Post.builder()
